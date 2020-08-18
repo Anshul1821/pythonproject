79cb77519e84d7405891f4469f43837a8f78b932
Anshul1821 <67154144+Anshul1821@users.noreply.github.com>
2020-08-18 11:06:29 +0530

    Add files via upload

--- prakrity.py
+++ prakrity.py
@@ -1,17 +1,20 @@
 work1=open("list1.txt","r")
 list1=[]
 for item in work1:
-    list1.append(item)
-  
+    items=item.strip()
+    list1.append(items)
+print(list1)  
 work2=open("list2.txt","r")
 list2=[]
 for item in work2:
-    list2.append(item)
+    items=item.strip()
+    list2.append(items)
 
 work3=open("list3.txt","r")
 list3=[]
 for item in work3:
-    list3.append(item)
+    items=item.strip()
+    list3.append(items)
 
 
 list_of_departments=["Content and Editorial","Creativity","Design and Video","Event Management and Publicity",
@@ -25,12 +28,16 @@ Em + tech + Design = 4 people. Which means only 4 people have applied for all th
 
 This is just an example, you gotta find all the combinations of 2 and 3. Please text me if you have any doubt.        
 '''
+
 for i in range(7):
-    for j in range(1,8):
+    for j in range(i+1,8):
         num=0
         list=[list_of_departments[i],list_of_departments[j]]
         print(list)
         for k in range(118):
-            if ([list1[k],list2[k]] or [list2[k],list3[k]] or [list1[k],list3[k]])==list:
-                num=num+1; 
-            print(num)    
+            a=[list1[k],list2[k]]
+            b=[list1[k],list3[k]] 
+            c=[list2[k],list3[k]]
+            if (a or b or c)==list:
+                num=num+1
+        print(num)               
